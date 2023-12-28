ea3ea26ce78411c2b3573e1e1ad06d25c90cb78e
Max Rustle <csmoke.mk@gmail.com>
2023-12-28 18:25:27 +0000

    exercise 3-7 completed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
@@ -67,8 +70,7 @@ app.post('/api/persons', (request, response) => {
         response.json(person)
         
     } else {
-        const errMess = { error: 'name must be unique' }
-        response.status(400).json(errMess)
+        response.status(400).send({ error: 'name must be unique' })
     }
 })
 
