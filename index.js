340aef5e8722b7308394b3be093aec5150e9f1d5
Max Rustle <csmoke.mk@gmail.com>
2024-01-17 16:48:15 +0000

    exercise 3-11 completed

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 morgan.token('mydata', (req, res) => JSON.stringify(req.body))
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :mydata'))
 
